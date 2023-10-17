894fe07ef1481a2cccfa2ad4c7a5469b7e82f34d
fdfiroz <firozahamed0@gmail.com>
2023-10-18 03:58:49 +0600

    Product business logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -25,7 +25,88 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const productsCollection = client.db("AutomotiveDB").collection("products");
+        app.post("/api/add-product", async (req, res) => {
+            const newProduct = req.body;
+            console.log('adding new product: ', newProduct);
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+            
+    })
+    //searching for products 
+    app.post("/api/search-products", async (req, res) => {
+        const searchProduct = req.body;
+        console.log('searching for product: ', searchProduct);
+        const result = await productsCollection.find(searchProduct).toArray();
+        res.send(result);
+    })
+    app.get("/api/products", async (req, res) => {
+        const cursor = productsCollection.find({});
+        const products = await cursor.toArray();
         
+        res.send( products);
+    })
+    //pagination 
+    app.post("/api/products/:page", async (req, res) => {
+        const page = req.params.page;
+        const limit = req.body || 10;
+        const skip = (page - 1) * limit;
+        const cursor = productsCollection.find({}).skip(skip).limit(limit);
+        const products = await cursor.toArray();
+        res.send( products);
+    })
+    //update product
+    app.put("/api/update-product/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedProduct = req.body;
+        console.log('updating product: ', id);
+        const filter = { _id: new ObjectId(id) };
+
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                name: updatedProduct.name,
+                price: updatedProduct.price,
+                quantity: updatedProduct.quantity,
+                description: updatedProduct.description,
+                img: updatedProduct.img,
+                brand: updatedProduct.brand,
+                category: updatedProduct.category,
+                rating: updatedProduct.rating,
+                suggestion: updatedProduct.suggestion
+            },
+        };
+        const result = await productsCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    })
+
+    //all uniq category names im a array and send to client side
+    app.get("/api/categories", async (req, res) => {
+        const cursor = productsCollection.find({});
+        const products = await cursor.toArray();
+        const categories = [];
+        products.forEach(product => {
+            if (!categories.includes(product.category)) {
+                categories.push(product.category);
+            }
+        });
+        res.send(categories);
+    })
+
+
+    
+
+
+
+    // add bulk products
+    app.post("/add-products", async (req, res) => {
+        const newProducts = req.body;
+        console.log('adding new products: ', newProducts);
+        const result = await productsCollection.insertMany(newProducts);
+        res.send(result);
+    })
+
+    //sending the result to the client side
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
