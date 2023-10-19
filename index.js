c7db935bcfa25273e9c5c504b24787213d2d856a
fdfiroz <firozahamed0@gmail.com>
2023-10-19 19:20:58 +0600

    Update Route links

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -23,30 +24,37 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+         await client.connect();
         const productsCollection = client.db("AutomotiveDB").collection("products");
-        app.post("/api/add-product", async (req, res) => {
+        app.post("/product", async (req, res) => {
             const newProduct = req.body;
             console.log('adding new product: ', newProduct);
             const result = await productsCollection.insertOne(newProduct);
             res.send(result);
             
     })
+    //show product by Uid
+    app.post("/product-by-uid", async (req, res) => {
+        const uid = req.body.uid;
+        const cursor = productsCollection.find({uid: uid});
+        const products = await cursor.toArray();
+        res.send(products);
+    })
     //searching for products 
-    app.post("/api/search-products", async (req, res) => {
+    app.post("/search-products", async (req, res) => {
         const searchProduct = req.body;
         console.log('searching for product: ', searchProduct);
         const result = await productsCollection.find(searchProduct).toArray();
         res.send(result);
     })
-    app.get("/api/products", async (req, res) => {
+    app.get("/all-products", async (req, res) => {
         const cursor = productsCollection.find({});
         const products = await cursor.toArray();
         
         res.send( products);
     })
     //pagination 
-    app.post("/api/products/:page", async (req, res) => {
+    app.post("/products/:page", async (req, res) => {
         const page = req.params.page;
         const limit = req.body || 10;
         const skip = (page - 1) * limit;
@@ -55,7 +63,7 @@ async function run() {
         res.send( products);
     })
     //update product
-    app.put("/api/update-product/:id", async (req, res) => {
+    app.put("/update-product/:id", async (req, res) => {
         const id = req.params.id;
         const updatedProduct = req.body;
         console.log('updating product: ', id);
@@ -80,35 +88,32 @@ async function run() {
     })
 
     //all uniq category names im a array and send to client side
-    app.get("/api/categories", async (req, res) => {
-        const cursor = productsCollection.find({});
-        const products = await cursor.toArray();
-        const categories = [];
-        products.forEach(product => {
-            if (!categories.includes(product.category)) {
-                categories.push(product.category);
-            }
-        });
-        res.send(categories);
-    })
-
+    app.get("/categories", async (req, res) => {
+        try {
+            const categories = await productsCollection.distinct("category");
+            res.json(categories);
+        } catch (error) {
+            console.error("Error fetching categories:", error);
+            res.status(500).send("Internal Server Error");
+        }
+    });
 // add to cart
     const cartCollection = client.db("AutomotiveDB").collection("cart");
-    app.post("/api/add-to-cart", async (req, res) => {
+    app.post("/add-to-cart", async (req, res) => {
         const newProduct = req.body;
         console.log('adding new product: ', newProduct);
         const result = await cartCollection.insertOne(newProduct);
         res.send(result);
     })
     //get cart by email
-    app.post("/api/cart", async (req, res) => {
+    app.post("/user-cart", async (req, res) => {
         const email = req.body.email;
-        const cursor = cartCollection.find({email: email});
+        const cursor = cartCollection.findOne({email: email});
         const products = await cursor.toArray();
         res.send(products);
     })
     //delete from cart by email
-    app.delete("/api/delete-from-cart/:id", async (req, res) => {
+    app.delete("/cart-product/:id", async (req, res) => {
         const id = req.params.id;
         const result = await cartCollection.deleteOne({_id: new ObjectId(id)});
         res.send(result);
@@ -117,14 +122,65 @@ async function run() {
 
 
 
-    // add bulk products
-    app.post("/add-products", async (req, res) => {
-        const newProducts = req.body;
-        console.log('adding new products: ', newProducts);
-        const result = await productsCollection.insertMany(newProducts);
+    // // add bulk products
+    // app.post("/products", async (req, res) => {
+    //     const newProducts = req.body;
+    //     console.log('adding new products: ', newProducts);
+    //     const result = await productsCollection.insertMany(newProducts);
+    //     res.send(result);
+    // })
+
+    //userinfo
+    const userInfoCollection = client.db("AutomotiveDB").collection("userInfo");
+    app.post("/user", async (req, res) => {
+        const newUserInfo = req.body;
+        console.log('adding new user info: ', newUserInfo);
+        const result = await userInfoCollection.insertOne(newUserInfo);
         res.send(result);
     })
+    //get user info by email
+    app.post("/user-info", async (req, res) => {
+        const email = req.body.email;
+        const cursor = userInfoCollection.find({email: email});
+        const userInfo = await cursor.toArray();
+        res.send(userInfo);
+    })
+    //update user info
+    app.patch("/user", async (req, res) => {
+        const user = req.body;
+        console.log('updating user info: ', user);
+        const filter = { email: user.email }
+        const updateDoc = {
+            $set: {
+                uid: user.uid,
+                emailVerified: user.emailVerified,
+                lastSignInTime: user.lastSignInTime,
+                photoURL: user.photoURL,
 
+            }
+        }
+        const result = await userInfoCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    })
+    app.put("/user", async (req, res) => {
+        const user = req.body;
+        console.log('updating user info: ', user);
+        console.log('updating user info: ', user);
+        const filter = { uid: user.uid }
+        const updateDoc = {
+            $set: {
+                name: user.name,
+                email: user.email,
+                photoURL: user.photoURL,
+                uid: user.uid,
+                emailVerified: user.emailVerified,
+                lastSignInTime: user.lastSignInTime,
+                createdAt: user.createdAt,
+            }
+        }
+        const result = await userInfoCollection.updateOne(filter, updateDoc, {upsert: true});
+        res.send(result);   
+    })
     //sending the result to the client side
 
 
@@ -140,7 +196,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-        res.send('Brand Server Running...')
+        res.send(`Brand Server Running...
+        <br>
+        <a href="/products">Products</a>
+        `)
 });
 
 app.listen(port, () => {
